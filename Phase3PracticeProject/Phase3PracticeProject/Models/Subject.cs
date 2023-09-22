95e0c4ab7fbc0d6ab82c64b747a22d8ce5595402
kiruthigarajendran1903 <kiruthiga2807@gmail.com>
2023-09-22 19:03:38 +0530

    practiceproject commit

---
+++ Phase3PracticeProject/Phase3PracticeProject/Models/Subject.cs
@@ -0,0 +1,8 @@
+﻿namespace Phase3PracticeProject.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Phase3PracticeProject/Phase3PracticeProject/Models/Teacher.cs
@@ -0,0 +1,9 @@
+﻿namespace Phase3PracticeProject.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TeachingSubject { get; set; }
+    }
+}
